c75c161b1e6ce257a35776130a859edc679923a8
sangjinsu <climbplant39@kakao.com>
2023-11-05 00:10:31 +0900

    ✨ Firebase Project

---
+++ src/firebase.ts
@@ -0,0 +1,14 @@
+import {initializeApp} from "firebase/app";
+import config from "../config.json"
+
+const firebaseConfig = {
+    apiKey: config.apiKey,
+    authDomain: config.authDomain,
+    projectId: config.projectId,
+    storageBucket: config.storageBucket,
+    messagingSenderId: config.messagingSenderId,
+    appId: config.appId,
+};
+
+
+const app = initializeApp(firebaseConfig);
