efed3de2010551c445bb9f222500003a2966c2de
sangjinsu <climbplant39@kakao.com>
2024-01-27 20:40:22 +0900

    ✨ Create Navigator

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,86 @@
-import {Outlet} from "react-router-dom";
+import {Link, Outlet, useNavigate} from "react-router-dom";
+import styled from "styled-components";
+import {auth} from "../firebase";
 
+const Wrapper = styled.div`
+    display: grid;
+    gap: 50px;
+    padding: 50px 0;
+    width: 100%;
+    max-width: 860px;
+    grid-template-columns: 1fr 4fr;
+`
+
+const Menu = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+`
+
+const MenuItem = styled.div`
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: 2px solid white;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    svg {
+        width: 30px;
+        fill: white;
+    }
+    &.log-out {
+        border-color: tomato;
+        svg {
+            fill: tomato;
+        }
+    }
+`
 
 export default function Layout() {
+    const navigate = useNavigate();
+    const onLogout = async ()  => {
+        const ok = confirm("Are you want to log out?")
+        if (ok){
+            await auth.signOut()
+            navigate("login")
+        }
+    }
+
     return (
-        <>
-            <h1>layout</h1>
+        <Wrapper>
+            <Menu>
+                <Link to="/">
+                    <MenuItem>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                             stroke="currentColor" className="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                  d="M15.75 9V5.25A2.25 2.25 0 0 0 13.5 3h-6a2.25 2.25 0 0 0-2.25 2.25v13.5A2.25 2.25 0 0 0 7.5 21h6a2.25 2.25 0 0 0 2.25-2.25V15M12 9l-3 3m0 0 3 3m-3-3h12.75"/>
+                        </svg>
+                    </MenuItem>
+                </Link>
+
+                <Link to='/profile'> <MenuItem>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                        <path fill-rule="evenodd"
+                              d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                              clip-rule="evenodd"/>
+                    </svg>
+                </MenuItem></Link>
+
+
+                <MenuItem className='log-out' onClick={onLogout}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                        <path fill-rule="evenodd"
+                              d="M7.5 3.75A1.5 1.5 0 0 0 6 5.25v13.5a1.5 1.5 0 0 0 1.5 1.5h6a1.5 1.5 0 0 0 1.5-1.5V15a.75.75 0 0 1 1.5 0v3.75a3 3 0 0 1-3 3h-6a3 3 0 0 1-3-3V5.25a3 3 0 0 1 3-3h6a3 3 0 0 1 3 3V9A.75.75 0 0 1 15 9V5.25a1.5 1.5 0 0 0-1.5-1.5h-6Zm5.03 4.72a.75.75 0 0 1 0 1.06l-1.72 1.72h10.94a.75.75 0 0 1 0 1.5H10.81l1.72 1.72a.75.75 0 1 1-1.06 1.06l-3-3a.75.75 0 0 1 0-1.06l3-3a.75.75 0 0 1 1.06 0Z"
+                              clip-rule="evenodd"/>
+                    </svg>
+                </MenuItem>
+
+            </Menu>
             <Outlet/>
-        </>
+        </Wrapper>
     )
 }
